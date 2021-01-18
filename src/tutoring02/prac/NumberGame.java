efc8681f9cf9345ba6d1e257205de1ca7b0f3116
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-01-18 22:26:06 +0900

    숫자맞추기(예외처리)

--- src/tutoring02/prac/NumberGame.java
+++ src/tutoring02/prac/NumberGame.java
@@ -5,8 +5,23 @@ public class NumberGame {
         int randNum = (int)(Math.random() * 100)+1;
         System.out.println("1 - 100 사이의 정수를 생성했습니다.");
         System.out.println(randNum);
-        NumberChecker numberChecker = new NumberChecker(randNum);
-        numberChecker.input();
-        numberChecker.checkNum();
+        NumberChecker numberChecker = new NumberChecker();
+        int input = numberChecker.input();
+
+        while (true) {
+            if (input == randNum) {
+                System.out.println("맞췄습니다.");
+                break;
+            } else {
+                if (input >= 1 && input <= 100) {
+                    if (input > randNum) {
+                        System.out.println("더 작은 수입니다.");
+                    } else {
+                        System.out.println("더 큰 수입니다.");
+                    }
+                } 
+                input = numberChecker.input();
+            }
+        }
     }
 }
